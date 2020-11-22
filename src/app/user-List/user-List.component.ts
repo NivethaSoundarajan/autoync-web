c4609c3a9d678b9cad94d7ab817063801738f437
RamyaPrimeMover <62927365+RamyaPrimeMover@users.noreply.github.com>
2020-11-22 15:41:34 +0530

    export

--- src/app/user-List/user-List.component.ts
+++ src/app/user-List/user-List.component.ts
@@ -1,11 +1,11 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild,ElementRef} from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatSort} from '@angular/material/sort';
 import { ActivatedRoute,Router } from '@angular/router';
 import { viewValues } from './modal';
 import { AutoSyncService } from '../../service';
-
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-user-List',
@@ -19,6 +19,7 @@ export class UserListComponent implements OnInit {
   dataSource = new MatTableDataSource<viewValues>();  
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
+  @ViewChild('TABLE') table: ElementRef;
 
  ngOnInit():void {
    var self = this;
@@ -30,6 +31,16 @@ export class UserListComponent implements OnInit {
       },
       (err) => {},
       () => { });
+ }
+ exportAsExcel()
+ {
+   const ws: XLSX.WorkSheet=XLSX.utils.table_to_sheet(this.table.nativeElement);//converts a DOM TABLE element to a worksheet
+   ws['!cols'][6] = { hidden: true };
+   const wb: XLSX.WorkBook = XLSX.utils.book_new();
+   XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+   /* save to file */
+   XLSX.writeFile(wb, 'UserList_'+new Date()+'.xlsx');
+
  }
 }
 
